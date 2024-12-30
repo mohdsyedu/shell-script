dbbcf073cec1b33a5a7665ab3cce2e132b074739
mohdsyedu <syedu061993@gmail.com>
2024-12-30 03:52:44 -0800

    Conversation with Arguments Added

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hi $PERSON2"
+echo "${PERSON2}:: Hello ${PERSON1}" #we can use like this also here
+echo "$PERSON1:: How are you doing..? $PERSON2"
+echo "$PERSON2:: Im Good bro how are you..?"

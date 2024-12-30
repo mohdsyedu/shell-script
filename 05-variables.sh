57de8048bd793bdb066b7ac2a65dc811957eff5d
mohdsyedu <syedu061993@gmail.com>
2024-12-30 04:11:36 -0800

    Username and password with read and -s added

---
+++ 05-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Please Enter your Username::"  #here entered text is added as a value to the  variable 
+read -s USERNAME #here entered text is a variable 
+echo "Entered UserName is:: $USERNAME"
+echo "Please Enter the Password::"
+read -s PASSWORD

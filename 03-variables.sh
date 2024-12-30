385e01dc9ca632431e5748dc037d87210cc9d0ea
mohdsyedu <syedu061993@gmail.com>
2024-12-30 03:37:51 -0800

    Conversation with variables Added

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1="RAMESH"
+PERSON2="SURESH"
+
+echo "$PERSON1:: Hi $PERSON2"
+echo "$PERSON2:: Hello $PERSON1"
+echo "$PERSON1:: How are you doing..? $PERSON2"
+echo "$PERSON2:: Im Good bro how are you..?"

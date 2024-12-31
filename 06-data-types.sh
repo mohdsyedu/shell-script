37192c9df513f707bf444dc6e7f11ae7c5f4ab83
mohdsyedu <syedu061993@gmail.com>
2024-12-31 03:53:04 -0800

    Code changes for datatypes Added

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -5,7 +5,7 @@ NUMBER2=$2
 
 TIMESTAMP=$(date)
 
-echo "script executed at:: $TIMESTAMP "
-SUM=($($NUMBER1+$NUMER2))
+echo "script executed at:: $TIMESTAMP"
+SUM=$(($NUMBER1+$NUMBER2))
 
 echo "sum of $NUMBER1 and $NUMBER2 :: is $SUM"

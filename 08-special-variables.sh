be8aa3d94fa444941848862012dcdbe209eed1db
mohdsyedu <syedu061993@gmail.com>
2024-12-31 04:31:00 -0800

    Special variables Added

---
+++ 08-special-variables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "all variables: $@"
+echo "number of variables: $#"
+echo "script Name : $0"
+echo "present working directory: $PWD"
+echo "HOME DIRECTORY OF A CURRENT USER : $HOME"
+echo "WHICH USER EXECUTING THE SCRIPT : $USER"
+echo "PROCESS ID OF THE CURRENT SCRIPT : $$"
+
+SLEEP 60 & #BACK GROUND COMMAND WE ARE USING TO CHECK THE BELOW COMMAND
+
+echo "PROCESS ID OF THE LAST COMMAND RUNNNING IN BACKGROUND : $!"
+
+
+

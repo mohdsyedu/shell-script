29f3245be27ee1766817513752f808d555c627ca
mohdsyedu <syedu061993@gmail.com>
2025-01-03 04:43:54 -0800

    Added LOGS to the current file with changes as update color to nornmal

--- 13-logs.sh
+++ 13-logs.sh
@@ -9,6 +9,7 @@ USERID=$(id -u) # Here we fetches the user id of the current user if sudo then i
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+N="\e[0m"
 LOGS_FOLDER="/var/log/shell-script-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1 )
 TIMIESTAMP=$(date +%Y-%m-%d-%H-%M-%S) # yyyy/mm/dd/HRS/MIN/SEC
@@ -19,10 +20,10 @@ VALIDATE()
 {
     if [ $1 -ne 0 ]
     then 
-       echo -e "$2 INSTALLATION....$R IS FAILURE"
+       echo -e "$2 INSTALLATION....$R IS FAILURE $N"
        exit 1 # After executing the above statement return with exit status as 1 other than 0  which is "failure"
     else 
-       echo -e "$2 INSTALLATION IS $G SUCCESS"
+       echo -e "$2 INSTALLATION IS $G SUCCESS $N"
     fi # End of If
 }
 
@@ -41,7 +42,7 @@ then
        VALIDATE $? "Mysql"
 
     else
-      echo -e "MYSQL Is already $Y Installed In your System.."
+      echo -e "MYSQL Is already $Y Installed In your System..$N"
     fi # End of If
 
    ############## To Install GIT into the linux server ##############
@@ -52,11 +53,11 @@ then
        dnf install git -y &>>$LOG_FILE_NAME
        VALIDATE $? "Git"
     else
-      echo -e "GIT Is already $Y Installed In your System.."
+      echo -e "GIT Is already $Y Installed In your System..$N"
     fi # End of If
 
 
 else
-      echo -e "$R ERROR:: You must have sudo access to execute this script"
+      echo -e "$R ERROR:: You must have sudo access to execute this script$N" #After practice nack to normal color
 fi # End of If
 

09d7ca6987eb991efef4099db709b1f9c15b630a
mohdsyedu <syedu061993@gmail.com>
2025-01-06 06:15:25 -0800

    Added Back-up scripts for source and destination folders

--- 18-backup-script.sh
+++ 18-backup-script.sh
@@ -63,7 +63,23 @@ if [ -n "$FILES" ] # true if there are files to zip
   echo "Files are $FILES"
   ZIP_FILE="$DEST_DIR/app-log-$TIMESTAMP.zip"
   find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ "$ZIP_FILE"
-  
+  if [ -f "$ZIP_FILE" ]
+  then 
+    echo -e "Successfully created file for the files older than $DAYS"
+
+            while read -r filepath # here filepath is the variable name, you can give any name
+            do
+                    echo "Deleting file: $filepath" &>>$LOG_FILE_NAME
+                    rm -rf $filepath
+                    echo "Deleted file: $filepath"
+            done <<< $FILES
+
+  else
+
+   echo -e "$R Error ::: $N Failed to create zip files "
+   exit 1
+
+   fi
 else 
 
    echo "No Files found older than $DAYS"
